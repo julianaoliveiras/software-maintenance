24bd4370a9f06d4a7e6167f559e1eaaa827992aa
Juliana Oliveira <h4bituei.o@gmail.com>
2021-11-04 18:14:45 -0300

    C1- Correção dos links no final da tela
    C1- corresponde em corrigir os links do rodapé do projeto Identificador de Recitas.

--- identificador-receitas/src/componets/rodape/index.js
+++ identificador-receitas/src/componets/rodape/index.js
@@ -14,33 +14,33 @@ function Rodape (){
                         <div className="rodape-menu"> 
                             <strong className="rodape-titulo">Termos</strong>
                             <ul>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">Docs</Link></li>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">Suporte Informações</Link></li>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">Privacidade</Link></li>
-                                <li className="rodape-menu-item"><Link to="#"className="rodape-link">Gerenciar cookies</Link></li>
+                                <li className="rodape-menu-item"><a href="https://github.com/julianaoliveiras/software-maintenance/tree/main/identificador-receitas" target="_blank" className="rodape-link">Docs</a></li>
+                                <li className="rodape-menu-item"><a href="https://github.com/julianaoliveiras/software-maintenance/tree/main/identificador-receitas" target="_blank" className="rodape-link">Suporte Informações</a></li>
+                                <li className="rodape-menu-item"><a href="https://github.com/julianaoliveiras/software-maintenance/tree/main/identificador-receitas" target="_blank" className="rodape-link">Privacidade</a></li>
+                                <li className="rodape-menu-item"><a href="https://github.com/julianaoliveiras/software-maintenance/tree/main/identificador-receitas" target="_blank" className="rodape-link">Gerenciar cookies</a></li>
                             </ul>
                         </div>
                         <div className="rodape-menu">
                             <strong className="rodape-titulo">Paginas</strong>
                             <ul>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">GitHub</Link></li>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">LinkedIn</Link></li>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">Facebook</Link></li>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">Youtube</Link></li>
+                                <li className="rodape-menu-item"><a href="https://github.com/julianaoliveiras/" target="_blank" className="rodape-link">GitHub</a></li>
+                                <li className="rodape-menu-item"><a href="https://www.linkedin.com/in/oliveira-juliana-/" target="_blank" className="rodape-link">LinkedIn</a></li>
+                                <li className="rodape-menu-item"><a href="https://www.facebook.com/julliana.oliveira.9674" target="_blank" className="rodape-link">Facebook</a></li>
+                                <li className="rodape-menu-item"><a href="https://www.youtube.com/" target="_blank" className="rodape-link">Youtube</a></li>
                             </ul>
                         </div>
                         <div className="rodape-menu">
                             <strong className="rodape-titulo">Contato</strong>
                             <ul>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">Fale Conosco</Link></li>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">Endeços</Link></li>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">SAC</Link></li>
-                                <li className="rodape-menu-item"><Link to="#" className="rodape-link">Dúvidas</Link></li>
+                                <li className="rodape-menu-item"><Link to="/contato" className="rodape-link">Fale Conosco</Link></li>
+                                <li className="rodape-menu-item"><Link to="/contato" className="rodape-link">Endeços</Link></li>
+                                <li className="rodape-menu-item"><Link to="/contato" className="rodape-link">SAC</Link></li>
+                                <li className="rodape-menu-item"><Link to="/sobre" className="rodape-link">Dúvidas</Link></li>
                             </ul>
                         </div>
                         <div className="limpar"></div>
                         <div className="autor">
-                            <p> Por <Link to="#" className="fim">Juliana</Link></p>
+                            <p> Por <a href="https://github.com/julianaoliveiras/" target="_blank" className="fim">Juliana</a></p>
                         </div>
                     </div>
         </div>
